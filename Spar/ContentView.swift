e0e2c972e38cc69c78dbb45ceeba0fc03a78dbbc
Milo4uk <wlir0tn@gmail.com>
2023-08-14 11:27:01 +0200

    Initial Commit

---
+++ Spar/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Spar
+//
+//  Created by Mila ✨ on 14.08.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Spar/SparApp.swift
@@ -0,0 +1,17 @@
+//
+//  SparApp.swift
+//  Spar
+//
+//  Created by Mila ✨ on 14.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct SparApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
